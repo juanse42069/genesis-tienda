343c00b6a6021b53b29f4a49e681bcc1a824e05a
juanse42069 <juansebasescobatg@gmail.com>
2025-05-02 16:58:15 -0500

    actualizacion del sitio web

---
+++ menu.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+});
