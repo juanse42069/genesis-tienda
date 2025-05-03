0d648ed16c8f1a63eb432843c50b78f7b4e7634b
juanse42069 <juansebasescobatg@gmail.com>
2025-05-03 00:36:47 -0500

    actualizacion del sitio web

--- menu.js
+++ menu.js
@@ -12,4 +12,39 @@ document.addEventListener('DOMContentLoaded', () => {
       navMenu.classList.remove("show");
     }
   });
+
+  // Restaura el efecto de hover en las imágenes de las cuadrículas
+  document.querySelectorAll('.product-image').forEach(image => {
+    const hoverSrc = image.dataset.hover;
+    const originalSrc = image.src;
+
+    image.addEventListener('mouseenter', () => image.src = hoverSrc);
+    image.addEventListener('mouseleave', () => image.src = originalSrc);
+  });
+
+  // Habilita funcionalidad táctil para los sliders
+  document.querySelectorAll('.slider').forEach(slider => {
+    let startX = 0;
+    let endX = 0;
+
+    slider.addEventListener('touchstart', (e) => {
+      startX = e.touches[0].clientX;
+    });
+
+    slider.addEventListener('touchmove', (e) => {
+      endX = e.touches[0].clientX;
+    });
+
+    slider.addEventListener('touchend', () => {
+      if (startX > endX + 50) {
+        // Deslizar hacia la izquierda
+        const rightArrow = slider.querySelector('.arrow.right');
+        if (rightArrow) rightArrow.click();
+      } else if (startX < endX - 50) {
+        // Deslizar hacia la derecha
+        const leftArrow = slider.querySelector('.arrow.left');
+        if (leftArrow) leftArrow.click();
+      }
+    });
+  });
 });
